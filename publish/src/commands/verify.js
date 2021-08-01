355fca05431f392bd80ef230b0a24888af6becff
Albert Rodriguez <albert@oikos.cash>
2021-07-31 23:45:44 -0400

    verify script

--- publish/src/commands/verify.js
+++ publish/src/commands/verify.js
@@ -52,7 +52,7 @@ const verify = async ({ buildPath, network, deploymentPath }) => {
 	const tableData = [];
 
 	const contractNames = Object.keys(config);
-	const contractNames = ['Exchanger'];
+	//const contractNames = ['Exchanger'];
 	for (const name of contractNames) {
 		const { address } = deployment.targets[name];
 		// Check if this contract already has been verified.
